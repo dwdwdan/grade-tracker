4bf21a70e0580c890ba405c708778caae33d1fab
Daniel Walters <dwdwdan@gmail.com>
2022-05-31 18:12:24 +0100

    Use README as long description

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
+this_dir=Path(__file__).parent
+long_desc = (this_dir / "README.md").read_text()
 
 setup(
     name="grade-tracker",
     version="1.0.11",
     author="Dan Walters",
     author_email="dan.walters5@outlook.com",
-    description="A small example package",
-    url="https://github.com/pypa/sampleproject",
+    description="A CLI to track grades",
+    url="https://github.com/dwdwdan/grade-tracker",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.6",
-    py_modules=['cli'],
     install_requires=["xdg", "pyyaml"],
     entry_points={
         "console_scripts": [
                 "grade-tracker=grade_tracker.cli:main",
             ]
         },
+    long_description=long_desc,
+    long_description_content_typr="text/markdown",
 )
