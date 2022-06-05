d6e618d8213cabfaa3c9e4345cb2183c3cf0503d
Daniel Walters <dwdwdan@gmail.com>
2022-06-05 13:35:06 +0100

    tests for check_module_tree

--- tests/test_grade_tracker/test_cli.py
+++ tests/test_grade_tracker/test_cli.py
@@ -1,4 +1,5 @@
 from grade_tracker import cli as gt
+from unittest.mock import Mock
 import pytest
 
 
@@ -7,3 +8,61 @@ import pytest
 def test_trunc(num, precis, expected):
     assert(gt.trunc(num, precis) == expected)
 
+
+def test_check_module_tree_succeeds(monkeypatch):
+    module_dict = [
+        {
+            "module": "Module1",
+            "weighting": 25,
+        },
+        {
+            "module": "Module2",
+            "weighting": 75,
+            "modules": [
+                {
+                    "module": "Module3",
+                    "weighting": 20
+                },
+                {
+                    "module": "Module4",
+                    "weighting": 79
+                }
+            ]
+        }
+    ]
+    args = Mock()
+    args.total_weighting_tolerance = 5
+    monkeypatch.setattr(gt, "args", args, False)
+
+    result = gt.check_module_tree(module_dict)
+
+    assert(result is None)
+
+
+def test_check_module_tree_fails(monkeypatch):
+    module_dict = [
+        {
+            "module": "Module1",
+            "weighting": 25,
+        },
+        {
+            "module": "Module2",
+            "weighting": 75,
+            "modules": [
+                {
+                    "module": "Module3",
+                    "weighting": 10
+                },
+                {
+                    "module": "Module4",
+                    "weighting": 80
+                }
+            ]
+        }
+    ]
+    args = Mock()
+    args.total_weighting_tolerance = 5
+    monkeypatch.setattr(gt, "args", args, False)
+
+    with pytest.raises(SystemExit):
+        gt.check_module_tree(module_dict)
